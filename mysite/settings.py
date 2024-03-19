abe14b17c1e969d7d67efffbe13e593bd7e5698a
cherie <cheriemusimegmail.com>
2024-03-19 12:11:43 -0700

    update blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@ku&&8356q^oyhvitv4b7u2s4tf)1vvor5wo*=@+^v8wn(7(0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cherie.pythonanywhere.com"]
 
 
 # Application definition
